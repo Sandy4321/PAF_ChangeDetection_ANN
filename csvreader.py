54a748f2fa2b838d737f08f7e793618a425a5f5d
Alberto BEGUE <alberto.begue@telecom-paristech.fr>
2018-06-18 17:41:35 +0200

    Change the script csvreader.py into a function

--- csvreader.py
+++ csvreader.py
@@ -1,17 +1,20 @@
 import pandas
 import timetools as tt
 
-#Parameters
+"""Parameters of the function could be 
 file_to_the_csv = './example/11017.csv'
 column = 'rtt' #rtt or cp 
+"""
 
-
-#Read the csv file
-trace = pandas.read_csv(file_to_the_csv, sep=';', decimal=',')
-try:
-    x = [tt.string_to_datetime(i) for i in trace['epoch']]
-except TypeError:
-    x = [tt.epoch_to_datetime(i) for i in trace['epoch']]
-y = trace[column]
-y = y.as_matrix()
-y = y.astype(np.float)
+#Function
+def csv2list(file_to_the_csv, column)
+    #Read the csv file
+    trace = pandas.read_csv(file_to_the_csv, sep=';', decimal=',')
+    try:
+        x = [tt.string_to_datetime(i) for i in trace['epoch']]
+    except TypeError:
+        x = [tt.epoch_to_datetime(i) for i in trace['epoch']]
+    y = trace[column]
+    y = y.as_matrix()
+    y = y.astype(np.float)
+    return y
