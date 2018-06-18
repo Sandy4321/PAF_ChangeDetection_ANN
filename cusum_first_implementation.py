f354c9db4910624b139bb820ee6cd3aad96343cb
Alberto BEGUE <alberto.begue@telecom-paristech.fr>
2018-06-18 08:46:38 +0200

    Add a graphical view of the implementation of cusum

--- cusum_first_implementation.py
+++ cusum_first_implementation.py
@@ -1,14 +1,22 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 def cusum(values): #Values are the values of the time-serie we are studying.
     S=0
     average =  np.average(values) #average of the time-serie
     changes = [0 for i in range(len(values))] #Position of the changes.
-    
+    cusum = []
     for i in range(len(values)):
+        cusum.append(S)
         S = S + values[i]-average #The cumulative sum.
         if(abs(S) > 5): #I took 5 as a criteria of a cusum too different from zero but it is highly subjective. How do we choose this criteria ?
             changes[i] = 1
-        else
+        else:
             changes[i] = 0
+    
+    index = [i for i in range(len(values))]
+    
+    plt.plot(index,cusum,label='Cusum')
+    plt.plot(index,changes,'o',label='Position of the changes')
+    plt.show()
     return
