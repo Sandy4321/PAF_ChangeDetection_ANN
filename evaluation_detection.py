4519f9303eb7c6f76d65dabbc4fedfaf945fa8e6
Randa Moalla <randa.moalla@telecom-paristech.fr>
2018-06-19 14:41:14 +0200

    cdf of precision and recall for both methods cusum and baysian

--- evaluation_detection.py
+++ evaluation_detection.py
@@ -32,10 +32,9 @@ def evaluationDataSet(folder):
     csv.list2csv('./results/resultBaysian.csv', [file_csv, precisionB, recallB], ['fileName', 'precision', 'recall'])
     csv.list2csv('./results/resultCUSUM.csv', [file_csv, precisionC, recallC], ['fileName', 'precision', 'recall'])
 
-def cdf_precision():
-    precision=cusum_evaluation("./rtt_series/real_trace_labelled")[0]
+def cdf_precision_cusum():
+    precision=csv.csv2list('./results/resultCUSUM.csv','precision')
     cdf=[float(k+1)/len(precision) for k in range(len(precision))]
-    print cdf
     precision.sort()
     plt.plot(precision,cdf)
     plt.xlabel("precision")
@@ -43,6 +42,40 @@ def cdf_precision():
     plt.title("cdf of precision for cusum method")
     plt.show()
     return
-evaluationDataSet("./rtt_series/real_trace_labelled")
+
+def cdf_recall_cusum():
+    recall=csv.csv2list('./results/resultCUSUM.csv','recall')
+    cdf=[float(k+1)/len(recall) for k in range(len(recall))]
+    recall.sort()
+    plt.plot(recall,cdf)
+    plt.xlabel("recall")
+    plt.ylabel("cdf")
+    plt.title("cdf of recall for cusum method")
+    plt.show()
+    return
+
+def cdf_precision_baysian():
+    precision=csv.csv2list('./results/resultBaysian.csv','precision')
+    cdf=[float(k+1)/len(precision) for k in range(len(precision))]
+    precision.sort()
+    plt.plot(precision,cdf)
+    plt.xlabel("precision")
+    plt.ylabel("cdf")
+    plt.title("cdf of precision for cusum method")
+    plt.show()
+    return
+
+def cdf_recall_baysian():
+    recall=csv.csv2list('./results/resultBaysian.csv','recall')
+    cdf=[float(k+1)/len(recall) for k in range(len(recall))]
+    recall.sort()
+    plt.plot(recall,cdf)
+    plt.xlabel("recall")
+    plt.ylabel("cdf")
+    plt.title("cdf of recall for cusum method")
+    plt.show()
+    return
+
+#evaluationDataSet("./rtt_series/real_trace_labelled")
         
         
