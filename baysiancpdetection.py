b3d1d9b5714ef5e2b1e000db8c7e0c015e247d08
Pepper <targaryenpepper@gmail.com>
2018-06-19 12:45:22 +0200

    Test of the algorithm Baysian

--- baysiancpdetection.py
+++ baysiancpdetection.py
@@ -2,8 +2,8 @@ import changedetect as cpt
 import numpy as np
 
 #This function is to apply the algorithm of Baysian
-def cpt(values):
-	detectionIndex = cpt.cpt_np(y)
+def baysiancpt(values):
+	detectionIndex = cpt.cpt_np(values)
 	detectionBinairy = np.zeros(len(values))
 	for p in detectionIndex:
 		detectionBinairy[p] = 1
