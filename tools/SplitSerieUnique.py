67307bc057f4d58c7028a1c3e694a56e95b15e03
liangwangENST <40291207+liangwangENST@users.noreply.github.com>
2018-06-25 11:41:14 +0200

    split the original file to new file with (rtt,cp)

---
+++ tools/SplitSerieUnique.py
@@ -0,0 +1,51 @@
+
+import numpy as np
+
+np.random.seed(1337)  # for reproducibility
+import tools.csvio as csv
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import SimpleRNN, Activation, Dense
+from keras.optimizers import Adam
+import tools.csvio as csv_t
+import csv
+import os
+import sys
+'''
+to create the fichier of series split with number_x.csv and number_y.csv
+to use this fonction 1. you have to create a folder name "dataset_split"
+2. you have to take all the fichier.csv to the folder "dataset_original". For example 11017.csv.
+'''
+
+length=500
+path_original='./dataset_original/'
+path_generate="./dataset_split/"
+flag=0
+for (path_original, dirs, files) in os.walk(path_original):
+    for filename in files:
+
+        print(filename)
+        data_rtt_test=csv_t.csv2list(path_original+str(filename),"rtt")
+        data_cp_test=csv_t.csv2list(path_original+str(filename),"cp")
+        #write data x and y in nx.csv and ny.csv
+
+        for i in range(int(len(data_rtt_test)/length)):
+            val_cp = []
+            val_rtt = []
+            header=["rtt","cp"]
+            filename_x =  str(flag+i) +"xy"+ ".csv"
+            with open(path_generate+filename_x,'wt') as f:
+                writer = csv.writer(f)
+                begin=i*length
+                end=(i+1)*length
+                for val in data_rtt_test[begin:end]:
+                    val_rtt.append(val)
+                for val in data_cp_test[begin:end]:
+                    val_cp.append(val)
+                val=[val_rtt,val_cp]
+                filename=path_generate+filename_x
+                print(filename)
+                csv_t.list2csv(filename,val,["rtt","cp"])
+            f.close()
+
+        flag = flag+int(len(data_cp_test)/1000)
