5afefba63bfca6a3124e723f5addb4829e61c98d
Pepper <targaryenpepper@gmail.com>
2018-06-27 11:08:05 +0000

    pre-treat: substraction of min

--- predictWithNeuroNet.py
+++ predictWithNeuroNet.py
@@ -8,21 +8,25 @@ import tools.evaluation as eval
 import os 
 
 # load json and create model
-json_file = open('detectWithNeuroNet.json', 'r')
+json_file = open('detectWithNeuroNetWithPreTreat.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("detectWithNeuroNet.h5")
+loaded_model.load_weights("detectWithNeuroNetWithPreTreat.h5")
 print("Loaded model from disk")
 
-'''
+
 # Test for an input
 #test = np.array([189.21973,189.105955,189.0866,189.19152,189.18938,189.21477,189.363065,189.060735,189.018825,189.497595,189.460615,194.76307,196.72467,189.280095,189.014585,189.02478,189.03338,189.087215,88.99757,189.041955])
 #test = csvio.csv2list("rtt_series/real_trace_labelled/11119.csv", "rtt")
-fileName = 'rtt_series/valid_data/5011.csv'
+fileName = 'rtt_series/valid_data/5008.csv'
 test = csvio.csv2list(fileName, "rtt", sep=';', decimal='.')
 testLen = len(test)
+min_test = min(test)
+for i in range(len(test)):
+    test[i] = test[i] - min_test
+
 test = test.reshape(1,testLen,1)
 temp = loaded_model.predict(test)
 temp = temp.reshape(testLen)
@@ -61,7 +65,7 @@ score = loaded_model.evaluate(X, Y, verbose=1)
 print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
 
 
-'''
+
 folderName = './rtt_series/artificial_dataset'
 file_csv = os.listdir(folderName)
 precision = []
