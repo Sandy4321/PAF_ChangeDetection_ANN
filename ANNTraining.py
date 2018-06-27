8d85648f38bc8cb537b08e8743486e0c505619eb
Pepper <targaryenpepper@gmail.com>
2018-06-27 15:07:46 +0000

    A great model for the prediction

--- ANNTraining.py
+++ ANNTraining.py
@@ -1,69 +1,38 @@
+'''
+This file use the bidirectional LSTM model to train the data set
+The data set will be separated by sequences with length of SAMPLE_LEN
+The number of the sequences will be determined by the division of the total length of the data set
+Every sequence of the data will be subtracted by minimun value of the sequence 
+'''
 import numpy as np
 import os
-# np.random.seed(42)
-# import benchmark as bch
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import TimeDistributed, Bidirectional
 from keras.models import Sequential
 import matplotlib.pyplot as plt
 import tools.csvio as csvio
-import neuroNetModelTool as md
+import ANNTool as md
 
 # global config
 SAMPLE_LEN = 100
 N_EPOCH = 200
 MTX = ['acc']
 folderName = './rtt_series/artificial_dataset'
-#folderName = './example'
 
 # Read data from the folder
 file_csv = os.listdir(folderName)
 x = []
 y = []
-min_x = 1000
-#SAMPLE_LEN = 0
 for f in file_csv:
     f = folderName + '/' + f
     temp = csvio.csv2list(f, 'trace', sep=',', decimal='.')
-    if min_x > min(temp):
-    	min_x = min(temp)
     x.append(temp)
     y.append(csvio.csv2list(f, 'cpt', sep=',', decimal='.'))
-# Get the biggest value of the sample length
-#    if len(temp) > SAMPLE_LEN:
-#        SAMPLE_LEN = len(temp)
-'''
-N_SAMPLE = len(x)
-
-print('N_SAMPLE = ')
-print(N_SAMPLE)
-
-print('SAMPLE_LEN = ')
-print(SAMPLE_LEN)
-# 0-padding for the samples whose length is less than SAMPLE_LEN
-data_x = []
-data_y = []
-for i in range(len(x)):
-    if len(x[i]) != SAMPLE_LEN:
-        data_x.extend(x[i].tolist())
-        data_x.extend(np.zeros(SAMPLE_LEN - len(x[i])).tolist())
-        data_y.extend(y[i].tolist())
-        data_y.append(1)
-        data_y.extend(np.zeros(SAMPLE_LEN - len(x[i]) - 1).tolist())
-    else:
-        data_x.extend(x[i].tolist())
-        data_y.extend(y[i].tolist())
-'''
 
 # add some zeros at the end
 data_x = []
 data_y = []
-
-# Pre-Treatment
-for i in range(len(x)):
-	for j in range(len(x[i])):
-		x[i][j] -= min_x
 
 for i in range(len(x)):
     data_x.extend(x[i].tolist())
@@ -72,11 +41,17 @@ for i in range(len(x)):
 N_SAMPLE = len(data_x) / SAMPLE_LEN
 reste = len(data_x) % SAMPLE_LEN
 if(reste != 0):
-	N_SAMPLE += 1
-	data_x.extend(np.zeros(SAMPLE_LEN - reste))
-	data_y.append(1)
-	data_y.extend(np.zeros(SAMPLE_LEN - reste -1))
-       
+    N_SAMPLE += 1
+    data_x.extend(np.zeros(SAMPLE_LEN - reste))
+    data_y.append(1)
+    data_y.extend(np.zeros(SAMPLE_LEN - reste - 1))
+
+# Pre-Treatment
+for i in range(N_SAMPLE):
+    temp = min(data_x[i * SAMPLE_LEN: ((i + 1) * SAMPLE_LEN)])
+    for j in range(i * SAMPLE_LEN, ((i + 1) * SAMPLE_LEN)):
+        data_x[j] -= temp
+
 # change list to np.array
 data_x = np.asarray(data_x)
 data_y = np.asarray(data_y)
@@ -87,11 +62,11 @@ data_x = data_x.reshape(N_SAMPLE, SAMPLE_LEN, 1)
 
 # prepare the model
 model = Sequential()
-model.add(Bidirectional(LSTM(15, return_sequences = True), input_shape = (None, 1)))
-model.add(TimeDistributed(Dense(1, activation = 'sigmoid')))
-model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = MTX)
-history = model.fit(data_x, data_y, validation_split = 0.2,
-                    epochs = N_EPOCH, batch_size = SAMPLE_LEN, verbose=1)
+model.add(Bidirectional(LSTM(32, return_sequences=True), input_shape=(None, 1)))
+model.add(TimeDistributed(Dense(1, activation='sigmoid')))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=MTX)
+history = model.fit(data_x, data_y, validation_split=0.2,
+                    epochs=N_EPOCH, batch_size=SAMPLE_LEN, verbose=1)
 print(model.summary())
 
 md.save_trained_model(model, fn='detectWithNeuroNetWithPreTreat')
--- ANNprediction.py
+++ ANNprediction.py
@@ -10,7 +10,8 @@ from keras.layers.recurrent import LSTM
 import numpy as np
 import tools.csvio as csvio
 import tools.evaluation as eval
-import os 
+import os
+
 
 def load_model(modelName):
     # load json and create model
@@ -23,6 +24,7 @@ def load_model(modelName):
     print("Loaded model from disk")
     return loaded_model
 
+
 def single_test(loaded_model, fileName='rtt_series/valid_data/5008.csv'):
     # Test for an input
     #test = np.array([189.21973,189.105955,189.0866,189.19152,189.18938,189.21477,189.363065,189.060735,189.018825,189.497595,189.460615,194.76307,196.72467,189.280095,189.014585,189.02478,189.03338,189.087215,88.99757,189.041955])
@@ -31,20 +33,20 @@ def single_test(loaded_model, fileName='rtt_series/valid_data/5008.csv'):
     testLen = len(test)
     min_test = min(test)
 
-    #Pre-Treatment
+    # Pre-Treatment
     for i in range(len(test)):
         test[i] = test[i] - min_test
-    test = test.reshape(1,testLen,1)
+    test = test.reshape(1, testLen, 1)
 
-    #Prediction
+    # Prediction
     temp = loaded_model.predict(test)
     temp = temp.reshape(testLen)
-    print("temp =")
-    print(temp)
+    #print("temp =")
+    #print(temp)
     res = np.zeros(testLen)
     for i in range(testLen):
-    	if temp[i] >= 0.5:
-    		res[i] = 1
+        if temp[i] >= 0.5:
+            res[i] = 1
     cp = csvio.csv2list(fileName, "cp", sep=';', decimal='.')
     print("cp =")
     print(cp)
@@ -52,6 +54,8 @@ def single_test(loaded_model, fileName='rtt_series/valid_data/5008.csv'):
     print("res =")
     print(res)
     print(sum(res))
+
+
 def model_evaluate(loaded_model, folderName='./rtt_series/valid_data'):
     # evaluate loaded model on test data
     SAMPLE_LEN = 100
@@ -63,15 +67,23 @@ def model_evaluate(loaded_model, folderName='./rtt_series/valid_data'):
         fileName = folderName + '/' + f
         test = csvio.csv2list(fileName, "rtt", sep=';', decimal='.')
         cp = csvio.csv2list(fileName, "cp", sep=';', decimal='.')
+
+        # Pre-Treatment
+        min_test = min(test)
+        for i in range(len(test)):
+            test[i] = test[i] - min_test
+
         X = np.concatenate([X, test])
-        Y = np.concatenate([Y, cp])    
+        Y = np.concatenate([Y, cp])
     X = X.reshape(N_SAMPLE, SAMPLE_LEN, 1)
     Y = Y.reshape(N_SAMPLE, SAMPLE_LEN, 1)
-    loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    loaded_model.compile(loss='binary_crossentropy',
+                         optimizer='adam', metrics=['accuracy'])
     score = loaded_model.evaluate(X, Y, verbose=1)
     print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
 
-def model_evaluate2precision_recall(loaded_model, folderName='./rtt_series/valid_data'):
+
+def model_evaluate2precision_recall(loaded_model, folderName='./rtt_series/valid_data', resultName='./results/resultNeuroNet_w.csv'):
     # generate precision and recall for every test file
     file_csv = os.listdir(folderName)
     precision = []
@@ -80,8 +92,14 @@ def model_evaluate2precision_recall(loaded_model, folderName='./rtt_series/valid
         fileName = folderName + '/' + f
         test = csvio.csv2list(fileName, "rtt", sep=';', decimal='.')
         cp = csvio.csv2list(fileName, "cp", sep=';', decimal='.')
+
+        # Pre-Treatment
+        min_test = min(test)
+        for i in range(len(test)):
+            test[i] = test[i] - min_test
+
         testLen = len(test)
-        test = test.reshape(1,testLen,1)
+        test = test.reshape(1, testLen, 1)
         temp = loaded_model.predict(test)
         temp = temp.reshape(testLen)
         res = np.zeros(testLen)
@@ -89,6 +107,7 @@ def model_evaluate2precision_recall(loaded_model, folderName='./rtt_series/valid
         for i in range(testLen):
             if temp[i] >= 0.5:
                 res[i] = 1
+        ''' Binary to index
         temp = []
         for i in range(len(res)):
             if(res[i] == 1):
@@ -99,13 +118,22 @@ def model_evaluate2precision_recall(loaded_model, folderName='./rtt_series/valid
             if(cp[i] - 1 < 0.001):
                 temp.append(i)
         cp = temp
+        '''
+        cp = cp.astype(int)
+        res = res.astype(int)
+        
+        print('res=')
+        print(res)
+        print('cp=')
+        print(cp)
+        #temp = eval.evaluation(cp, res)
+        #precision.append(temp["precision"])
+        #recall.append(temp["recall"])
+    #csvio.list2csv(resultName, [file_csv, precision, recall], ['fileName', 'precision', 'recall'])
+    print("Prediction & Recall saved.")
 
-        temp = eval.evaluation_window_adp(cp, res, 2)
-        precision.append(temp["precision"])
-        recall.append(temp["recall"])
-    csvio.list2csv('./results/resultNeuroNet_w.csv', [file_csv, precision, recall], ['fileName', 'precision', 'recall'])
 
 model = load_model("detectWithNeuroNetWithPreTreat")
-#single_test(model)
-#model_evaluate(model)
-model_evaluate2precision_recall(model)
+single_test(model)
+model_evaluate(model)
+#model_evaluate2precision_recall(model, resultName='./results/resultNeuroNet.csv')
